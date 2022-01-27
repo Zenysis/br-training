78a8feb31979a4b7a1c4e0034c0f812321b60d7d
Abby Beeler <abbybeeler@Abbys-Work-MacBook.local>
2022-01-26 17:08:24 -0800

    adding sim integration steps

---
+++ pipeline/brazil_covid/bin/sim/convert_cid_csv.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+import sys
+
+import pandas as pd
+import json
+from pylib.base.flags import Flags
+from slugify import slugify
+
+from log import LOG
+
+ALPHABET_LOCATION = {letter: i for i, letter in enumerate('ABCDEFGHIJKLMNOPQRSTUVWXYZ')}
+ALPHABET_LOOKUP = {i: letter for i, letter in enumerate('ABCDEFGHIJKLMNOPQRSTUVWXYZ')}
+
+CID_USED = set()
+
+def create_ids(cid_str):
+    start_str, end_str = cid_str.split('-')
+    start_l, start_n, start_sn = format_cid(start_str)
+    end_l, end_n, end_sn = format_cid(end_str, end=True)
+    start = CID(start_l, start_n, start_sn)
+    end = CID(end_l, end_n, end_sn)
+    indicators = []
+    while less_than(start, end, inclusive=True) and ALPHABET_LOCATION[start.letter] < 25:
+        current_id = start.to_id()
+        if current_id not in CID_USED:
+            CID_USED.add(current_id)
+            indicators.append(current_id)
+        start = start.add_one()
+    return indicators
+
+
+def less_than(start_CID, end_CID, inclusive=True):
+    if ALPHABET_LOCATION[start_CID.letter] < ALPHABET_LOCATION[end_CID.letter]:
+        return True
+    elif ALPHABET_LOCATION[start_CID.letter] == ALPHABET_LOCATION[end_CID.letter]:
+        if start_CID.number < end_CID.number:
+            return True
+        elif start_CID.number == end_CID.number:
+            if start_CID.subnumber < end_CID.subnumber:
+                return True
+            elif start_CID.subnumber == end_CID.subnumber:
+                return inclusive
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+
+def format_cid(cid, end=False):
+    letter = cid[0]
+    if '.' in cid[1:]:
+        number, subnumber = cid[1:].split('.')
+    else:
+        number = cid[1:]
+        subnumber = 0 if not end else 9
+    return letter, int(number), int(subnumber)
+
+class CID:
+    def __init__(self, letter, number, subnumber):
+        self.letter = letter
+        self.number = number
+        self.subnumber = subnumber
+
+    def to_id(self):
+        number = str(self.number)
+        if len(number) < 2:
+            number = f'0{number}'
+        return f'{self.letter.lower()}{number}{self.subnumber}'
+
+    def add_one(self):
+        if self.subnumber < 9:
+            return CID(self.letter, self.number, self.subnumber + 1)
+        elif self.number < 99:
+            return CID(self.letter, self.number + 1, 0)
+        else:
+            return CID(ALPHABET_LOOKUP[ALPHABET_LOCATION[self.letter] + 1], 0, 0)
+
+
+
+def separate_name_id(dataframe):
+    output = []
+    for row in dataframe['cid']:
+        cid, name = row.split(')', 1)
+        cid = cid.replace('(', '')
+        output.append({'cid': cid, 'name': name})
+    return pd.DataFrame(output)
+
+def create_group_mapping(row):
+    group_mapping = {
+        'name': row['name'],
+        'id': row['cid'].lower().replace('(', '').replace('.', ''),
+        'slug': slugify(row['name'], separator='_')
+    }
+    indicator_ids = create_ids(row['cid'])
+    group_mapping['indicators'] = indicator_ids
+    return group_mapping
+
+def main():
+    Flags.PARSER.add_argument(
+        '--input_file', type=str, required=True, help='Raw csv lookup of cause of death to id.'
+    )
+    Flags.PARSER.add_argument(
+        '--output_indicator_groups', type=str, required=True, help='File path for the cause of death indicators'
+    )
+    Flags.PARSER.add_argument(
+        '--cause_of_death_lookup', type=str, required=True, help='File path for the cause of death lookup'
+    )
+    Flags.InitArgs()
+    cause_of_death_df = separate_name_id(pd.read_csv(Flags.ARGS.input_file))
+    LOG.info(cause_of_death_df.head(10))
+    groups = []
+    for i, row in cause_of_death_df.iterrows():
+        if '-' in row['cid']:
+            format_group = create_group_mapping(row)
+            if len(format_group) > 0:
+                groups.append(format_group)
+    cause_of_death_indicators = [{'id': f"sim_cause_of_death_{g['slug']}", 'text': g['name'].strip()} for g in groups]
+    cause_of_death_group = {
+        'groupId': 'sim_cause_of_death',
+        'groupText': 'Causa da Morte',
+        'indicators': cause_of_death_indicators,
+    }
+    json_data = json.dumps([cause_of_death_group], indent=4)
+    with open(Flags.ARGS.output_indicator_groups, 'w') as output_json:
+        output_json.write(json_data)
+    lookup = [{'cid_id': cid_id, 'field_id': f"sim_cause_of_death_{g['slug']}"} for g in groups for cid_id in g['indicators']]
+    pd.DataFrame(lookup).to_csv(Flags.ARGS.cause_of_death_lookup, index=False)
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ pipeline/brazil_covid/bin/sim/convert_raw_sim.py
@@ -0,0 +1,299 @@
+#!/usr/bin/env python
+import sys
+
+import pandas as pd
+
+from pylib.base.flags import Flags
+
+from log import LOG
+from util.file.compression.lz4 import LZ4Writer
+
+INPUT_DATE_FORMAT = '%d%m%Y'
+
+ESC2010_COLUMN_MAPPING = {
+    '0': 'Sem escolaridade',
+    '1': 'Fundamental I (1ª a 4ª série)',
+    '2': 'Fundamental II (5ª a 8ª série)',
+    '3': 'Médio (antigo 2º Grau)',
+    '4': 'Superior incompleto',
+    '5': 'Superior completo',
+    '9': 'Ignorado',
+    # NOTE(stephen): We want the empty column value to be empty after transformation.
+    '': '',
+}
+
+YES_NO_RESPONSE_MAPPING = {
+    '1': 'Sim',
+    '2': 'Não',
+    '9': 'Ignorado',
+    '': '',
+}
+
+# Mapping from column name to a dictionary containing values that should be replaced in
+# that column.
+COLUMN_MAPPING_VALUES = {
+    'SEXO': {
+        '1': 'Masculino',
+        '2': 'Feminino',
+        '0': 'Ignorado'
+    },
+    'RACACOR': {
+        '1': 'Branca',
+        '2': 'Preta',
+        '3': 'Amarela',
+        '4': 'Parda',
+        '5': 'Indígena',
+        '': 'Ignorado',
+    },
+    'ESC2010': ESC2010_COLUMN_MAPPING,
+    'ESCMAE2010': ESC2010_COLUMN_MAPPING,
+    'GRAVIDEZ': {
+        '1': 'Única',
+        '2': 'Dupla',
+        '3': 'Tripla e mais',
+        '9': 'Ignorada',
+        # NOTE(stephen): Empty string likely means the person was not pregnant.
+        '': '',
+    },
+    'LOCOCOR': {
+        '1': 'Hospital',
+        '2': 'Outros estabelecimentos de saúde',
+        '3': 'Domicílio',
+        '4': 'Via pública',
+        '5': 'Outros',
+        '6': 'Aldeia indígena',
+        '9': 'Ignorado',
+    },
+    'PARTO': {
+        '1': 'Vaginal',
+        '2': 'Cesáreo',
+        '9': 'Ignorado',
+        '': '',
+    },
+    'OBITOPARTO': {
+        '1': 'Antes',
+        '2': 'Durante',
+        '3': 'Depois',
+        '9': 'Ignorado',
+        '': '',
+    },
+    'TPMORTEOCO': {
+        '1': 'Na gravidez',
+        '2': 'No parto',
+        '3': 'No abortamento',
+        '4': 'Até 42 dias após o término do parto',
+        '5': 'De 43 dias a 1 ano após o término da gestação',
+        '8': 'Não ocorreu nestes períodos',
+        '9': 'Ignorado',
+        '': '',
+    },
+    'ASSISTMED': YES_NO_RESPONSE_MAPPING,
+    'NECROPSIA': YES_NO_RESPONSE_MAPPING,
+    'ACIDTRAB': YES_NO_RESPONSE_MAPPING,
+    'OBITOGRAV': YES_NO_RESPONSE_MAPPING,
+    'ATESTANTE': {
+        '1': 'Assistente',
+        '2': 'Substituto',
+        '3': 'IML',
+        '4': 'SVO',
+        '5': 'Outro',
+        '': '',
+    },
+    'CIRCOBITO': {
+        '1': 'Acidente',
+        '2': 'Suicídio',
+        '3': 'Homicídio',
+        '4': 'Outros',
+        '9': 'Ignorado',
+        '': '', # Not a violent death.
+    },
+    'FONTE': {
+        '1': 'Ocorrência policial',
+        '2': 'Hospital',
+        '3': 'Família',
+        '4': 'Outra',
+        '9': 'Ignorado',
+        '': '',
+    },
+    'OBITOPUERP': {
+        '1': 'Sim, até 42 dias após o parto',
+        '2': 'Sim, de 43 dias a 1 ano',
+        '3': 'Não',
+        '9': 'Ignorado',
+        '': '',
+    },
+    'FONTEINV': {
+        '1': 'Comitê de Morte Materna e/ou Infantil',
+        '2': 'Visita domiciliar / Entrevista família',
+        '3': 'Estabelecimento de Saúde / Prontuário',
+        '4': 'Relacionado com outros bancos de dados',
+        '5': 'SVO',
+        '6': 'IML',
+        '7': 'Outra fonte',
+        '8': 'Múltiplas fontes',
+        '9': 'Ignorado',
+        '': '',
+    },
+}
+
+# Mapping from residence location to the output name for those locations.
+RESIDENCE_LOCATION_COLUMN_RENAME = {
+    'RegionName': 'RegionNameRes',
+    'StateName': 'StateNameRes',
+    'HealthRegionName': 'HealthRegionNameRes',
+    'MunicipalityName': 'MunicipalityNameRes'
+}
+
+# Mapping from death occurrence location to the output name for those locations.
+OCCURENCE_LOCATION_COLUMN_RENAME = {
+    'RegionName': 'RegionNameOcor',
+    'StateName': 'StateNameOcor',
+    'HealthRegionName': 'HealthRegionNameOcor',
+    'MunicipalityName': 'MunicipalityNameOcor'
+}
+
+INPUT_DATE_COLUMN = 'DTOBITO'
+INPUT_MUNICIPALITY_RESIDENCE_COLUMN = 'CODMUNRES'
+INPUT_MUNICIPALITY_OCCURRENCE_COLUMN = 'CODMUNOCOR'
+INPUT_CAUSE_OF_DEATH_COLUMN = 'CAUSABAS'
+
+INPUT_REQUIRED_COLUMNS = [
+    INPUT_DATE_COLUMN,
+    INPUT_MUNICIPALITY_RESIDENCE_COLUMN,
+    INPUT_MUNICIPALITY_OCCURRENCE_COLUMN,
+    INPUT_CAUSE_OF_DEATH_COLUMN,
+    *COLUMN_MAPPING_VALUES.keys(),
+]
+
+OUTPUT_CAUSE_OF_DEATH_FIELD = 'cause_of_death_field_id'
+INPUT_CAUSE_OF_DEATH_FIELD = 'field_id'
+
+def main():
+    Flags.PARSER.add_argument(
+        '--input_file', type=str, required=True, help='Raw SIM dataset to convert'
+    )
+    Flags.PARSER.add_argument(
+        '--municipality_code_mapping_file',
+        type=str,
+        required=True,
+        help='CSV file containing a mapping from municipality code to the full hierarchy'
+    )
+    Flags.PARSER.add_argument(
+        '--output_file', type=str, required=True, help='Converted file location'
+    )
+    Flags.PARSER.add_argument(
+        '--cause_of_death_codes_csv', type=str, required=True, help='File path for cause of death codes to fields'
+    )
+    Flags.InitArgs()
+
+    LOG.info('Reading input file into dataframe')
+    df = pd.read_csv(
+        Flags.ARGS.input_file,
+        sep=';',
+        dtype=str,
+        keep_default_na=False,
+        usecols=INPUT_REQUIRED_COLUMNS
+    )
+    LOG.info('Finished reading input file')
+
+    input_count = len(df)
+    LOG.info('Number of rows in input: %s', input_count)
+
+    # Convert the date column into a full date object using the input format.
+    df['date'] = pd.to_datetime(df[INPUT_DATE_COLUMN], format=INPUT_DATE_FORMAT)
+
+    LOG.info('Starting remapping column values to human readable strings')
+    for column_name, mapping_values in COLUMN_MAPPING_VALUES.items():
+        df[column_name] = df[column_name].replace(mapping_values)
+    LOG.info('Finished remapping column values')
+
+    # TODO(stephen): Add in code for states that is missing in CSV. We are seeing
+    # that municipality is ignored for some rows (~717) so we need to account for
+    # this in the mapping.
+    # https://www.ibge.gov.br/explica/codigos-dos-municipios.php
+    LOG.info('Reading municipality code mapping file')
+    municipality_df = pd.read_csv(Flags.ARGS.municipality_code_mapping_file, dtype=str)
+    LOG.info('Finished reading municipalities')
+
+
+    # Merge municipality information into primary dataframe for location of residence.
+    LOG.info('Matching residence municipality code to full locations')
+    df = (df
+        .merge(municipality_df, left_on=INPUT_MUNICIPALITY_RESIDENCE_COLUMN, right_on='MunicipalityCodeShort')
+        .rename(columns=RESIDENCE_LOCATION_COLUMN_RENAME)
+        .drop(columns=['MunicipalityCodeShort', 'MunicipalityCodeLong'])
+    )
+
+    # Merge municipality information into primary dataframe for location of death.
+    LOG.info('Matching death occurrence municipality code to full locations')
+    df = (df
+        .merge(municipality_df, left_on=INPUT_MUNICIPALITY_OCCURRENCE_COLUMN, right_on='MunicipalityCodeShort')
+        .rename(columns=OCCURENCE_LOCATION_COLUMN_RENAME)
+        .drop(columns=['MunicipalityCodeShort', 'MunicipalityCodeLong'])
+    )
+    after_merge_count = len(df)
+    assert input_count == after_merge_count, \
+        'Some rows were dropped after municipality code merging! Original rows: %s, New rows: %s' % (input_count, after_merge_count)
+
+    LOG.info('Finished matching municipalities')
+
+    LOG.info('Starting cause of death load')
+    cause_of_death_df = pd.read_csv(Flags.ARGS.cause_of_death_codes_csv)
+    shortened_cid_lookup = {}
+    for _, row in cause_of_death_df.iterrows():
+        shortened_cid_lookup[row['cid_id'][:-1]] = row[INPUT_CAUSE_OF_DEATH_FIELD]
+    shortened_df = []
+    for key, val in shortened_cid_lookup.items():
+        shortened_df.append({'cid_id': key, INPUT_CAUSE_OF_DEATH_FIELD: val})
+    shortened_df = pd.DataFrame(shortened_df)
+    cause_of_death_df = pd.concat([cause_of_death_df, shortened_df])
+
+    LOG.info(cause_of_death_df.head(10))
+    df[INPUT_CAUSE_OF_DEATH_COLUMN] = [s.lower() for s in df[INPUT_CAUSE_OF_DEATH_COLUMN]]
+    LOG.info(df[INPUT_CAUSE_OF_DEATH_COLUMN].unique())
+    
+    df = (df
+        .merge(cause_of_death_df, left_on=INPUT_CAUSE_OF_DEATH_COLUMN, right_on='cid_id')
+        .rename(columns={INPUT_CAUSE_OF_DEATH_FIELD: OUTPUT_CAUSE_OF_DEATH_FIELD})
+        .drop(columns=['cid_id', INPUT_CAUSE_OF_DEATH_COLUMN])
+    )
+    after_merge_count = len(df)
+    assert input_count == after_merge_count, \
+        'Some rows were dropped after cause of death code merging! Original rows: %s, New rows: %s' % (input_count, after_merge_count)
+
+
+    LOG.info('Building numeric field columns')
+    # Build a unique numeric field for each dimension value in all the columns that we care
+    # about. This will allow the user to query for a specific value without grouping.
+    for column, dimension_mapping in COLUMN_MAPPING_VALUES.items():
+        for dimension_value in dimension_mapping.values():
+            # If there is no value for this dimension in the dataframe, we don't need to
+            # create a field for it.
+            if not dimension_value:
+                continue
+
+            field_name = f'*field_{column} - {dimension_value}' # 'ESCMAE2010 - Sem escolaridade'
+            df_row_filter = df[column] == dimension_value # df['ESCMAE2010'] == 'Sem escolaridade'
+
+            # Initialize the field to 0 since all rows that do not match the dimension value
+            # should not be counted.
+            df[field_name] = 0
+
+            # Set all rows that match the filter to 1.
+            df.loc[df_row_filter, field_name] = 1
+    LOG.info('Finished building numeric field columns')
+    for dimension_value in df[OUTPUT_CAUSE_OF_DEATH_FIELD].unique():
+        field_name = f'*field_{OUTPUT_CAUSE_OF_DEATH_FIELD} - {dimension_value}' # 'ESCMAE2010 - Sem escolaridade'
+        df_row_filter = df[OUTPUT_CAUSE_OF_DEATH_FIELD] == dimension_value # df['ESCMAE2010'] == 'Sem escolaridade'
+        df[field_name] = 0
+        df.loc[df_row_filter, field_name] = 1
+
+    LOG.info('Writing the output CSV')
+    with LZ4Writer(Flags.ARGS.output_file) as output_file:
+        df.to_csv(output_file, index=False)
+
+    LOG.info('Finished writing output CSV')
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
